9be1de528cd08716b996f3149e1adae1ae7799ca
serflosa <sergiofloriano@gmail.com>
2016-04-30 03:32:15 +0200

    First commit

---
+++ Debug/main.pp
@@ -0,0 +1,4 @@
+# FIXED
+main.obj: ../main.c
+main.obj: C:/Program\ Files\ (x86)/Texas\ Instruments/ccsv4/msp430/include/msp430f5519.h
+main.obj: C:/Program\ Files\ (x86)/Texas\ Instruments/ccsv4/msp430/include/in430.h
---
+++ main.c
@@ -0,0 +1,5 @@
+#include <msp430f5519.h>
+
+int main(void) {
+	return 0;	
+}
